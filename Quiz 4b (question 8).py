edcc18dd2f41c634302fdf4d4c18ec91edeb32d1
kkec4er <kkecher@outlook.com>
2019-06-29 21:03:22 +1000

    New files

---
+++ Quiz 4b (question 8).py
@@ -0,0 +1,37 @@
+pos = t * v - each draw event
+v = t * a - each draw event
+a = same vector
+
+import simplegui
+
+circle_pos = [10, 20]
+circle_move = [0, 0]
+
+def key_down_handler(key):
+    global circle_move
+    if key == simplegui.KEY_MAP['up']:
+        circle_move = [3, 0.7]
+    if key == simplegui.KEY_MAP['down']:
+        circle_move = [-3, -0.7]
+        
+def key_up_handler(key):
+    global circle_move
+    if key == simplegui.KEY_MAP['up'] or key == simplegui.KEY_MAP['down']:
+        circle_move = [0, 0]
+    
+
+def draw_handler(canvas):
+    circle_pos[0]+= circle_move[0]
+    circle_pos[1]+= circle_move[1]
+    canvas.draw_polygon([(50, 50), (180, 50), (180,140), (50, 140)],
+                        3, 'Blue', 'Lime')
+    canvas.draw_circle(circle_pos, 5, 1, 'Orange', 'Yellow')
+    if int(circle_pos[0]) in range(50, 180) and int(circle_pos[1]) in range(50, 140):
+        canvas.draw_text('Boom!', (75, 100), 30, 'Red')
+
+
+frame = simplegui.create_frame('Do they match?', 300, 300)
+frame.set_draw_handler(draw_handler)
+frame.start()
+frame.set_keydown_handler(key_down_handler)
+frame.set_keyup_handler(key_up_handler)
